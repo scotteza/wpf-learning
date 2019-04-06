0e51786338df22f0ad4d94a7aa51f955ca5fdc3b
Scott Edwards <scotteza@gmail.com>
2019-04-06 12:25:40 +0100

    Use DataContext to perform simple one-way binding

---
+++ DataBindingOneWay/DataBindingOneWay/Employee.cs
@@ -0,0 +1,17 @@
+﻿namespace DataBindingOneWay
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public static Employee GetEmployee()
+        {
+            return new Employee
+            {
+                Name = "Bob",
+                Title = "Developer"
+            };
+        }
+    }
+}
