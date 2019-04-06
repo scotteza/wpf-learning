4c04944d4d558d3cb444ae26eb9a34663a028d1e
Scott Edwards <scotteza@gmail.com>
2019-04-06 12:02:32 +0100

    Learn how to handle events using event handlers

--- SimpleForm/SimpleForm/MainWindow.xaml.cs
+++ SimpleForm/SimpleForm/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace SimpleForm
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _selectedJob = "(None)";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +29,46 @@ namespace SimpleForm
 
         private void ButtonSave_OnClick(object sender, RoutedEventArgs e)
         {
-            
+            var sb = new StringBuilder();
+
+            sb.Append("Full Name: ");
+            sb.AppendLine(!string.IsNullOrWhiteSpace(TextBoxFullName.Text) ? TextBoxFullName.Text : "(None)");
+
+            sb.Append("Sex: ");
+            sb.AppendLine(RadioButtonSexMale.IsChecked != null && RadioButtonSexMale.IsChecked.Value ? "Male" : "Female");
+
+            sb.Append("Computer(s): ");
+            sb.Append(CheckBoxDesktop.IsChecked != null && CheckBoxDesktop.IsChecked.Value ? "Desktop" : "No Desktop");
+            sb.Append(", ");
+            sb.Append(CheckBoxLaptop.IsChecked != null && CheckBoxLaptop.IsChecked.Value ? "Laptop" : "No Laptop");
+            sb.Append(", ");
+            sb.AppendLine(CheckBoxTablet.IsChecked != null && CheckBoxTablet.IsChecked.Value ? "Tablet" : "No Tablet");
+
+            sb.Append("Job: ");
+            sb.AppendLine(_selectedJob);
+
+            sb.Append("Delivery Date: ");
+            sb.Append(CalendarPromisedDeliveryDate.SelectedDate != null
+                ? CalendarPromisedDeliveryDate.SelectedDate.Value.ToString("yyyy/MM/dd")
+                : "(None)");
+
+            MessageBox.Show(sb.ToString());
+        }
+
+        private void ComboBoxJob_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (var addedItem in e.AddedItems)
+            {
+                SelectJobIfValidComboBoxItem(addedItem);
+            }
+        }
+
+        private void SelectJobIfValidComboBoxItem(object addedItem)
+        {
+            if (addedItem is ComboBoxItem item)
+            {
+                _selectedJob = item.Content.ToString();
+            }
         }
     }
 }
